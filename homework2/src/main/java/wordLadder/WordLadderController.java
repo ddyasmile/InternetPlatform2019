3fe176c5bfeb09d649411ee4700dbdaa4660b8cc
ddyasmile <1271569621@qq.com>
2019-04-12 13:43:54 +0800

    spring security start

--- homework2/src/main/java/wordLadder/WordLadderController.java
+++ homework2/src/main/java/wordLadder/WordLadderController.java
@@ -1,6 +1,8 @@
 package wordLadder;
 
 import java.util.Stack;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,8 @@ public class WordLadderController {
     private final String dictionaryPath2 = "src/smalldict1.txt";
 
     @RequestMapping("/wordLadder")
-    public Stack<String> passLadder(@RequestParam(value="start", defaultValue="world") String start,
-                                    @RequestParam(value="end", defaultValue="world") String end)
+    public Stack<String> passLadder(@RequestParam(value="start", defaultValue="") String start,
+                                    @RequestParam(value="end", defaultValue="") String end)
     {
         WordLadder aLadder = new WordLadder(dictionaryPath1);
         aLadder.find_ladder(start, end);
--- homework2/src/test/java/wordLadder/WordLadderActuallerTest.java
+++ homework2/src/test/java/wordLadder/WordLadderActuallerTest.java
@@ -1,4 +1,72 @@
 package wordLadder;
 
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Stack;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"management.port=0"})
 public class WordLadderActuallerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Value("${local.management.port}")
+    private int mgt;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldReturn200WhenSendingRequestToManagementEndpoint() throws Exception {
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
+                "http://localhost:" + this.mgt + "/actuator/info", Map.class);
+
+        then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        Map<?, ?> info = (Map<?, ?>)(entity.getBody().get("app"));
+        then(info.get("name")).isEqualTo("spring-boot-wordLadder");
+        then(info.get("version")).isEqualTo("v1.0.0");
+    }
+
+    @Test
+    public void checkHealthStatus() throws Exception {
+        Map<?, ?> response = this.testRestTemplate.getForObject(
+                "http://localhost:" + this.mgt + "actuator/health", Map.class);
+
+        then(response.get("status")).isEqualTo("UP");
+    }
+
+    @Test
+    public void checkHttptrace() throws Exception {
+
+        String url = "http://localhost:" + this.port + "wordLadder";
+        this.testRestTemplate.getForObject(url, Stack.class);
+
+        Map<?, ?> trace = this.testRestTemplate.getForObject(
+                "http://localhost:" + this.mgt + "actuator/httptrace", Map.class);
+        Map<?, ?> request = (Map<?, ?>) ((Map<?, ArrayList<Map<?, ?>>>)trace).get("traces").get(0).get("request");
+        Map<?, ?> response = (Map<?, ?>) ((Map<?, ArrayList<Map<?, ?>>>)trace).get("traces").get(0).get("response");
+
+        then(((Map<?, ArrayList<?>>)trace).size()).isEqualTo(1);
+        then(request.get("method")).isEqualTo("GET");
+        then(response.get("status")).isEqualTo(200);
+    }
 }
